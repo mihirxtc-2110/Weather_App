ffe84724c5a3019eaf8f2d1746ddf064648fd95e
mihirxtc-2110 <mihirmenon21@gmail.com>
2023-12-10 22:48:55 +0530

    uploaded

---
+++ app.js
@@ -0,0 +1,14 @@
+getWeather = () => {
+    let weather = document.getElementById("weather").value;
+    console.log(weather);
+
+    let suggestion = document.getElementById("suggestion");
+
+    if (weather == "rainy") { 
+        suggestion.innerHTML = "<h2>It looks like weather is " + weather + ", Please carry your umbrealla ☔ with you.</h2>"
+    } else if (weather == "sunny") {
+        suggestion.innerHTML = "<h2>It looks like weather is " + weather + ", Please wear your sunglasees 😎.</h2>"
+    } else {
+        suggestion.innerHTML = "<h2>Please enter a valid weather condition, you entered " + weather +"</h2>";
+    }
+}
